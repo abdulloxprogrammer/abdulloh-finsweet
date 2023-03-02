00a56c4129f8ea9be703bb602efe2e4b60ae4aa1
abdulloxprogrammer <abdulloxr3545@gmail.com>
2023-03-02 05:44:17 -0800

    first commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+window.addEventListener("scroll", function () {
   toggleBacktop();
 });
 
